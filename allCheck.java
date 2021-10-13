f9b4acda8d0eeae855b2187130387600a5e5f01d
vinay <vinay01431998@gmail.com>
2021-10-13 18:39:47 +0530

    FirstCommit

--- allCheck.java
+++ allCheck.java
@@ -2,5 +2,6 @@ class allCheck{
     public static void main(String[] args){
         System.out.println("hello Incubye");
         System.out.println(3 + 9);
+        System.out.println("send");
     }
 }
